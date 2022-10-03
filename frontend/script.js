eb2df637bc980cce5ce7b3c244f2cfcbb63b0d94
molnarbalazsnandor <molnarbalazsnandor@gmail.com>
2022-10-04 00:32:02 +0200

    blink animation in css

--- frontend/script.js
+++
@@ -1,5 +0,0 @@
-var blink_speed = 1; // every 1000 == 1 second, adjust to suit
-var t = setInterval(function () {
-  var ele = document.getElementById("class1");
-  ele.style.visibility = ele.style.visibility == "hidden" ? "" : "hidden";
-}, blink_speed);
